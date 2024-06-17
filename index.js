552250233e622abae3c18e68998907ac8543d14b
Santosh Saroj <santoshsaroj0032@gmail.com>
2024-06-17 18:21:13 +0530

    17-06

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const path= require("path");
+const path = require("path");
 const express = require("express");
-
 
 const app = express();
 const PORT = 7000;
 
- app.set("view engine", "ejs");
- app.set("views", path.resolve("./views"));
+// Set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
 
- app.use(express.json());
+// Middleware to parse JSON requests
+app.use(express.json());
 
- app.get("/", (req, res)=>{
-    return res.render("homepage");
- })
+// Define the homepage route
+app.get("/", (req, res) => {
+    return res.render("index.ejs");
+});
 
-
- app.listen(PORT , ()=> console.log(`Server Started at PORT:7000`));
- 
+// Start the server
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
